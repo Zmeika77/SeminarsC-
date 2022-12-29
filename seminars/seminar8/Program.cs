e97863bb08a688f24e0a71c207d0847c604a37d4
yuliya <avdyukova.y.a@gmail.com>
2022-12-29 23:00:50 +0300

    ДЗ2

--- seminars/seminar8/Program.cs
+++ seminars/seminar8/Program.cs
@@ -59,3 +59,117 @@ void PrintArray(int[,] array)
         Console.WriteLine("");
     }
 }*/
+/*Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+которая будет находить строку с наименьшей суммой элементов.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке 
+и выдаёт номер строки с наименьшей суммой элементов: 1 строка*/
+/*Console.WriteLine("Введите размер квадратного массива");
+int massVol = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[massVol, massVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+int minsum = Int32.MaxValue;
+int indexLine = 0;
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum  = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        indexLine++;
+    }
+}
+Console.WriteLine("Строка с наименьшей суммой элементов под номером: " + (indexLine) + ", с суммой элементов равной: " + (minsum));
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          Console.Write(array[i, j] + "");
+        }  
+        Console.Write("]");
+        Console.WriteLine("");  
+        
+    }
+}*/
+/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18*/
+/*int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+} 
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            array[i, j] = new Random().Next(1, 5); 
+        }
+    }
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(0); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
+int size = InputInt("Размерность матриц: ");
+int[,] matrixA = new int[size, size];
+int[,] matrixB = new int[size, size];
+FillArrayRandomNumbers(matrixA);
+FillArrayRandomNumbers(matrixB);
+int[,] matrixC = new int[size, size];
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+       for (int k = 0; k < size; k++)
+       {
+         matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
+       }         
+    }
+}
+Console.WriteLine("Матрица - A");
+PrintArray(matrixA);
+Console.WriteLine();
+Console.WriteLine("Матрица - B");
+PrintArray(matrixB);
+Console.WriteLine();
+Console.WriteLine("Произведение матриц A*B");
+PrintArray(matrixC);*/
+
